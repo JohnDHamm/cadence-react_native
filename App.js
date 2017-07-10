ad6baaa9505ab10350ec01da571f5c0a2ecf501d
John Hamm <johndhamm1@gmail.com>
2017-07-10 17:05:51 -0500

    layout tap button

--- App.js
+++ App.js
@@ -1,23 +1,72 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+	render() {
+		return (
+			<View style={styles.container}>
+				<View style={styles.top}>
+					<Text>height: {Dimensions.get('window').height}</Text>
+				</View>
+				<View style={styles.tapContainer}>
+					<View style={styles.tapBtn}>
+						<Text style={styles.athleteName}>MAKENNA</Text>
+						<Text style={styles.readout}>0.75s / 105rpm</Text>
+						<Text style={styles.athleteName}> </Text>
+					</View>
+				</View>
+				<View style={styles.bottom} />
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#222',
+	},
+	top: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderColor: '#444',
+		borderWidth: 1,
+		borderStyle: 'solid'
+	},
+	tapContainer: {
+		flex: 5,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	tapBtn: {
+		// flex: 1,
+		width: Dimensions.get('window').width * 0.95,
+		height: Dimensions.get('window').width * 0.95,
+		backgroundColor: '#000',
+		borderStyle: 'solid',
+		borderColor: '#e2bb2d',
+		borderWidth: 2,
+		borderRadius: Dimensions.get('window').width * 0.475,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	athleteName: {
+		color: '#e2bb2d',
+		fontSize: 22
+	},
+	readout: {
+		color: '#fff',
+		fontSize: 37,
+		paddingTop: 17,
+		paddingBottom: 17
+	},
+	bottom: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderColor: '#444',
+		borderWidth: 1,
+		borderStyle: 'solid'
+	}
 });
