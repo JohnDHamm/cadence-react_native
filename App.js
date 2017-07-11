1ff97004661880475c210fee5b916a407aa3c59f
John Hamm <johndhamm1@gmail.com>
2017-07-10 23:24:27 -0500

    components wrapped in touchableOpacity components + tested

--- App.js
+++ App.js
@@ -1,32 +1,63 @@
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions, Image } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, Image, TouchableOpacity } from 'react-native';
 
 export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { pressedBtn: 'none' };
+	}
+
+	chooseAthlete() {
+		this.setState({pressedBtn: 'select athlete'});
+	}
+
+	getResults() {
+		this.setState({pressedBtn: 'get results'});
+	}
+
+	tap() {
+		this.setState({pressedBtn: 'tapped!'});
+	}
+
+	reset() {
+		this.setState({pressedBtn: 'reset'});
+	}
+
 	render() {
+
 		return (
 			<View style={styles.container}>
 				<View style={styles.top}>
-					<Image
-						style={styles.iconBtn}
-	          source={require('./img/ic_group.png')}
-	        />
-					<Image
-						style={styles.iconBtn}
-	          source={require('./img/ic_list.png')}
-	        />
+					<TouchableOpacity onPress={this.chooseAthlete.bind(this)}>
+						<Image
+							style={styles.iconBtn}
+							source={require('./img/ic_group.png')}
+						/>
+					</TouchableOpacity>
+					<TouchableOpacity onPress={this.getResults.bind(this)}>
+						<Image
+							style={styles.iconBtn}
+							source={require('./img/ic_list.png')}
+						/>
+					</TouchableOpacity>
 				</View>
 				<View style={styles.tapContainer}>
-					<View style={styles.tapBtn}>
-						<Text style={styles.athleteName}>MAKENNA</Text>
-						<Text style={styles.readout}>0.75s / 105rpm</Text>
-						<Text style={styles.athleteName}> </Text>
-					</View>
+					<TouchableOpacity onPress={this.tap.bind(this)}>
+						<View style={styles.tapBtn}>
+							<Text style={styles.athleteName}>MAKENNA</Text>
+							<Text style={styles.readout}>0.75s / 105rpm</Text>
+							<Text style={styles.athleteName}> </Text>
+						</View>
+					</TouchableOpacity>
 				</View>
 				<View style={styles.bottom}>
-					<Image
-						style={styles.resetBtn}
-	          source={require('./img/ic_highlight_off_444.png')}
-	        />
+					<TouchableOpacity onPress={this.reset.bind(this)}>
+						<Image
+							style={styles.resetBtn}
+							source={require('./img/ic_highlight_off.png')}
+						/>
+					</TouchableOpacity>
+					<Text style={styles.testText}>{this.state.pressedBtn}</Text>
 				</View>
 			</View>
 		);
@@ -35,7 +66,6 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
 	container: {
-		// flex: 1,
 		backgroundColor: '#222',
 	},
 	top: {
@@ -46,9 +76,6 @@ const styles = StyleSheet.create({
 		alignItems: 'center',
 		paddingLeft: 15,
 		paddingRight: 15,
-		borderColor: '#444',
-		borderWidth: 1,
-		borderStyle: 'solid'
 	},
 	iconBtn: {
 		width: 50,
@@ -57,7 +84,6 @@ const styles = StyleSheet.create({
 	tapContainer: {
 		width: Dimensions.get('window').width,
 		height: Dimensions.get('window').width,
-		// flex: 5,
 		justifyContent: 'center',
 		alignItems: 'center'
 	},
@@ -83,17 +109,17 @@ const styles = StyleSheet.create({
 		paddingBottom: 17
 	},
 	bottom: {
-		// flex: 1,
 		width: Dimensions.get('window').width,
 		height: (Dimensions.get('window').height - Dimensions.get('window').width) / 2,
 		justifyContent: 'center',
 		alignItems: 'center',
-		borderColor: '#444',
-		borderWidth: 1,
-		borderStyle: 'solid'
 	},
-	iconBtn: {
+	resetBtn: {
 		width: 40,
 		height: 40
+	},
+	testText: {
+		color: '#666',
+		fontSize: 18
 	}
 });
