259528c1380bd1449ee18d783c603aca96aeafd0
John Hamm <johndhamm1@gmail.com>
2017-07-13 14:25:08 -0500

    delete athlete with long press + confirm modal added

--- src/screens/athleteList.js
+++ src/screens/athleteList.js
@@ -1,23 +1,47 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView, Modal, Dimensions } from 'react-native';
 import { connect } from 'react-redux';
 import { Constants } from 'expo';
 import _ from 'lodash';
 
-import { getAthletes, setCurrentAthlete } from '../actions';
+import { getAthletes, setCurrentAthlete, deleteAthlete } from '../actions';
 
 
 class AthleteList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+			modalVisible: false,
+			athleteToDelete: ''
+		}
+	}
+
+	setModalVisible(visible) {
+		this.setState({modalVisible: visible});
+	}
 
 	selectAthlete(athleteName) {
 		this.props.setCurrentAthlete(athleteName);
 		this.props.navigation.navigate('Home');
 	}
 
+	_deleteAthlete() {
+		console.log("athlete to delete", this.state.athleteToDelete);
+		this.setModalVisible(false);
+		this.props.deleteAthlete(this.state.athleteToDelete);
+	}
+
 	renderAthleteList() {
 		return _.map(this.props.athletes, athlete => {
 			return (
-				<TouchableOpacity key={athlete.name} onPress={() => this.selectAthlete(athlete.name)}>
+				<TouchableOpacity
+					key={athlete.name}
+					onPress={() => this.selectAthlete(athlete.name)}
+					onLongPress={() => {
+						this.setState({ athleteToDelete: athlete.name })
+						this.setModalVisible(true)
+					}}
+				>
 					<Text style={styles.athleteText}>{athlete.name}</Text>
 				</TouchableOpacity>
 			);
@@ -40,6 +64,34 @@ class AthleteList extends React.Component {
 						</TouchableOpacity>
 					</View>
 				</ScrollView>
+
+				<Modal
+					style={styles.modal}
+					animationType={"slide"}
+					transparent={true}
+					visible={this.state.modalVisible}
+					onRequestClose={ () => {this.setModalVisible(!this.state.modalVisible)}}>
+					<View style={styles.modalContainer}>
+
+						<View style={styles.modalTitle}>
+							<Text style={styles.modalTitleText}>Delete {this.state.athleteToDelete}?</Text>
+						</View>
+
+						<View style={styles.modalBtnGroup}>
+							<TouchableOpacity onPress={ () => {
+								this.setModalVisible(!this.state.modalVisible)
+							}}>
+								<Text style={styles.cancelBtn}>CANCEL</Text>
+							</TouchableOpacity>
+							<TouchableOpacity onPress={ () => {
+								this._deleteAthlete()}}>
+								<Text style={styles.saveBtn}>DELETE</Text>
+							</TouchableOpacity>
+						</View>
+
+					</View>
+				</Modal>
+
 			</View>
 		);
 	}
@@ -73,6 +125,41 @@ const styles = StyleSheet.create({
 		color: '#ddd',
 		fontSize: 28,
 		paddingBottom: 12
+	},
+	modalContainer: {
+		marginTop: (Dimensions.get('window').height - 100) / 2,
+		marginBottom: (Dimensions.get('window').height - 100) / 2,
+		marginRight: 35,
+		marginLeft: 35,
+		flex: 1,
+		flexDirection: 'column',
+		justifyContent: 'space-around',
+		alignItems: 'center',
+		borderRadius: 5,
+		backgroundColor: 'rgba(255,255,255,0.95)'
+	},
+	modalTitle: {
+		flex: 1,
+		justifyContent: 'flex-end',
+	},
+	modalTitleText: {
+		color: '#444',
+		fontSize: 22
+	},
+	modalBtnGroup: {
+		flex: 1,
+		flexDirection: 'row',
+		justifyContent: 'space-around',
+		alignItems: 'center',
+		width: '80%'
+	},
+	cancelBtn: {
+		fontSize: 20,
+		color: '#888'
+	},
+	saveBtn: {
+		fontSize: 20,
+		color: 'green'
 	}
 });
 
@@ -80,38 +167,6 @@ function mapStateToProps({ athletes }) {
 	return { athletes };
 }
 
-export default connect(mapStateToProps, { getAthletes, setCurrentAthlete })(AthleteList);
+export default connect(mapStateToProps, { getAthletes, setCurrentAthlete, deleteAthlete })(AthleteList);
 
-				// <Modal
-				// 	style={styles.modal}
-				// 	animationType={"slide"}
-				// 	transparent={true}
-				// 	visible={this.state.modalVisible}
-				// 	onRequestClose={ () => {this.setModalVisible(!this.state.modalVisible)}}>
-				// 	<View style={styles.modalContainer}>
 
-				// 		<View style={styles.modalTitle}>
-				// 			<Text style={styles.modalTitleText}>enter athlete name</Text>
-				// 		</View>
-				// 		<View style={styles.modalInput}>
-				// 			<TextInput
-				// 				style={styles.modalInputText}
-				// 				onChangeText={(inputName) => this.setState({inputName})}
-				// 				value={this.state.inputName}
-				// 			/>
-				// 		</View>
-
-				// 		<View style={styles.modalBtnGroup}>
-				// 			<TouchableOpacity onPress={ () => {
-				// 				this.setModalVisible(!this.state.modalVisible)
-				// 			}}>
-				// 				<Text style={styles.cancelBtn}>CANCEL</Text>
-				// 			</TouchableOpacity>
-				// 			<TouchableOpacity onPress={ () => {
-				// 				this.addAthlete()}}>
-				// 				<Text style={styles.saveBtn}>SAVE</Text>
-				// 			</TouchableOpacity>
-				// 		</View>
-
-				// 	</View>
-				// </Modal>
